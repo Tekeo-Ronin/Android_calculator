e5c7ee674ef930927e0a1872da7bfae19af13146
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2023-04-10 19:01:31 +0200

    create View

--- app/src/main/java/de/vogella/android/calculator/MainActivity.java
+++ app/src/main/java/de/vogella/android/calculator/MainActivity.java
@@ -2,9 +2,17 @@ package de.vogella.android.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+    Double operand = null;
+    String lastOperation = "=";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
